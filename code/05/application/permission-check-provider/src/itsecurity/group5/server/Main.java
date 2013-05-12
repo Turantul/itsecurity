15a461d63939088dcf7edaa306281eece8fa1cb9
Michael Heil <michael.heil@chello.at>
2013-05-12 11:22:40 +0200

    Initial structure

---
+++ code/05/application/permission-check-provider/src/itsecurity/group5/server/Main.java
@@ -0,0 +1,9 @@
+package itsecurity.group5.server;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ code/05/application/securedobject/src/itsecurity/group5/client/Console.java
@@ -0,0 +1,9 @@
+package itsecurity.group5.client;
+
+public class Console
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
